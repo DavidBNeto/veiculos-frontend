571d1803c545312e965477a1b31e6f0c21667145
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-04-21 23:31:07 -0300

    fix: component responsiveness

--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -2,8 +2,9 @@ import styled from "styled-components"
 
 const DropdownArea = styled.div`
   display: flex;
-  width: 15rem;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  overflow: hidden;
 `
+
 export default DropdownArea
