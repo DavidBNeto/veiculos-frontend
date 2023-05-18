5047bc6eef06d0015c27400054eb9b86e0382999
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-05-17 22:56:31 -0300

    feat: finished skeleton

--- src/pages/ViewPdf/index.tsx
+++ src/pages/ViewPdf/index.tsx
@@ -1,5 +1,16 @@
+import PageWrapper from "./styles"
+import GlobalStyle from "../../styles/styles"
+import HomeButton from "../../components/HomeButton"
+
 const ViewPdf = () => {
-  return <div>View PDF</div>
+  return (
+    <PageWrapper>
+      <HomeButton />
+      <div>Lista de PDFs</div>
+      <div>Grade de Carros</div>
+      <GlobalStyle />
+    </PageWrapper>
+  )
 }
 
 export default ViewPdf
