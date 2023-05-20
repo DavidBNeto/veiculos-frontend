e0a385001e585a055aca3be61aab13ccd40abef0
Brenda Pereira <brendacamara@outlook.com>
2023-05-20 16:27:00 -0300

    feat: create-tabs-component

--- src/pages/ViewPdf/index.tsx
+++ src/pages/ViewPdf/index.tsx
@@ -1,5 +1,43 @@
+import TabsView from "../../components/TabsView"
+
+const mockPDFList = [
+  {
+    id: 1,
+    title: "PDF 1",
+    data: {
+      field1: "any_value",
+    },
+  },
+  {
+    id: 2,
+    title: "PDF 2",
+    data: {
+      field1: "any_value",
+    },
+  },
+  {
+    id: 3,
+    title: "PDF 3",
+    data: {
+      field1: "any_value",
+    },
+  },
+  {
+    id: 4,
+    title: "PDF 4",
+    data: {
+      field1: "any_value",
+    },
+  },
+]
+
 const ViewPdf = () => {
-  return <div>View PDF</div>
+  return (
+    <div>
+      View PDF
+      <TabsView pdfList={mockPDFList} />
+    </div>
+  )
 }
 
 export default ViewPdf
