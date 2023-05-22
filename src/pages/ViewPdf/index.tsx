08414790711a244e0d7ff5b1b66a589091156276
Kevin Ribas <ribaskevin@gmail.com>
2023-05-22 14:34:22 -0300

    feat: save button

--- src/pages/ViewPdf/index.tsx
+++ src/pages/ViewPdf/index.tsx
@@ -1,4 +1,5 @@
-import PageWrapper from "./styles"
+import { t } from "i18next"
+import { PageWrapper, SaveButton } from "./styles"
 import GlobalStyle from "../../styles/styles"
 import HomeButton from "../../components/HomeButton"
 
@@ -8,6 +9,9 @@ const ViewPdf = () => {
       <HomeButton />
       <div>Lista de PDFs</div>
       <div>Grade de Carros</div>
+      <SaveButton variant="contained" color="primary">
+        {t("view.saveButton")}
+      </SaveButton>
       <GlobalStyle />
     </PageWrapper>
   )
