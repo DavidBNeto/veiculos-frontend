69adedeff1ff765b95073ca46d3d83b4025334cf
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-05-22 18:47:26 -0300

    US02-TK04-listagem-pdfs (#2)

--- src/pages/ViewPdf/index.tsx
+++ src/pages/ViewPdf/index.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom"
 import PageWrapper from "./styles"
 import GlobalStyle from "../../styles/styles"
 import HomeButton from "../../components/HomeButton"
+import PDFList from "../../components/PDFList"
 
 const ViewPdf = () => {
   const navigate = useNavigate()
@@ -14,7 +15,7 @@ const ViewPdf = () => {
   return (
     <PageWrapper>
       <HomeButton onClick={onHomeClick} />
-      <div>Lista de PDFs</div>
+      <PDFList />
       <div>Grade de Carros</div>
       <GlobalStyle />
     </PageWrapper>
